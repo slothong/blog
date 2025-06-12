4da1bf13d548ab12750b5b65a6a42debfbc0d8d3
slothong <hep93@naver.com>
2025-06-12 14:04:42 +0900

    feat: syntax highlighting

--- src/components/markdown-viewer/markdown-viewer.ts
+++ src/components/markdown-viewer/markdown-viewer.ts
@@ -1,5 +1,7 @@
 import { Component, computed, input } from '@angular/core';
-import { marked } from 'marked';
+import { Marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
+import hljs from 'highlight.js';
 
 @Component({
   selector: 'app-markdown-viewer',
@@ -8,7 +10,20 @@ import { marked } from 'marked';
 export class MarkdownViewerComponent {
   readonly markdown = input<string>();
 
+  private readonly marked = new Marked(
+    markedHighlight({
+      emptyLangClass: 'hljs',
+      langPrefix: 'hljs language-',
+      highlight(code, lang, info) {
+        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+        const value = hljs.highlight(code, { language }).value;
+        console.log(value);
+        return value;
+      },
+    })
+  );
+
   protected readonly html = computed(() => {
-    return marked.parse(this.markdown() ?? '');
+    return this.marked.parse(this.markdown() ?? '');
   });
 }
